bd6c5e05a6392e49c0e09922b7d9f450cfe4e9bb
Guiller <learsi.guil2010@gmail.com>
2022-01-20 14:04:05 -0400

    Configuracion

--- src/com/pgt360/decoder/NettyDecoder.java
+++ src/com/pgt360/decoder/NettyDecoder.java
@@ -10,6 +10,7 @@ import io.netty.channel.ChannelHandlerContext;
 import io.netty.handler.codec.ByteToMessageDecoder;
 import static java.lang.System.in;
 import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
 import java.util.List;
 
 /**
@@ -20,11 +21,13 @@ public class NettyDecoder extends ByteToMessageDecoder{
     private final Charset charset = Charset.forName("UTF-8");
     @Override
     protected void decode(ChannelHandlerContext chc, ByteBuf bb, List<Object> list) throws Exception {
+        String message = "";
         System.out.println("Decodificando mensaje");
         if(bb.readableBytes()<1){
             return;
         }
-        //int length = bb.readInt();
+        String s = bb.readCharSequence(bb.readableBytes(),Charset.forName("utf-8")).toString();
+        list.add(s);
     }
     
 }
